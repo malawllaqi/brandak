f4802ef87178871fc91125db03d12c644915ba54
malawllaqi <malawllaqi@gmail.com>
2025-09-18 02:41:03 +0300

    landing page

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,30 +1,33 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
-
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+import "@workspace/ui/globals.css";
+import { fonts } from "@workspace/ui/lib/fonts";
+import { cn } from "@workspace/ui/lib/utils";
+import { Providers } from "@/components/providers";
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
+    <html
+      className={cn(fonts, "scroll-smooth")}
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body>
+        <Providers>
+          <main className="relative min-h-svh w-full">
+            <div
+              className="absolute inset-0 z-0"
+              style={{
+                background:
+                  "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(244, 114, 182, 0.25), transparent 70%), #000000",
+              }}
+            />
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
+import { HeroSection } from "./_components/hero";
+import { SiteHeader } from "./_components/site-header";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="relative">
+      <SiteHeader />
+      <HeroSection />
+      <div className="h-screen" />
+      <div className="h-screen" />
     </div>
-  )
+  );
 }
